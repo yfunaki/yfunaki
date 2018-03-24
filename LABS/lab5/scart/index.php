d9e56b6a6453521730ba29511dfd9a14096f7715
Kaila Funaki <yfunaki@csumb.edu>
2018-03-24 18:42:35 +0000

    adding lab5

--- LABS/lab5/scart/index.php
+++ LABS/lab5/scart/index.php
@@ -90,7 +90,7 @@
         <hr>
             CST 336 Internet Programming 2018&copy; Funaki <br />
             <strong> Disclaimer: </strong> This information on this webpage is used only for academic purposes. <br />
-            <img src="img/buddy_verified.png alt-"Buddy Verified Badge" />
+            <img src="img/buddy_verified.png" alt-"Buddy Verified Badge" />
             
     </footer>
 </html>
